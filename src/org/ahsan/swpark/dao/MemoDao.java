9ffcaf91626be78a41565bf94ff1d99ba67d8647
danuplus <danuplus@gmail.com>
2013-01-23 16:17:33 +0900

    Update src/org/ahsan/swpark/dao/MemoDao.java

--- src/org/ahsan/swpark/dao/MemoDao.java
+++ src/org/ahsan/swpark/dao/MemoDao.java
@@ -13,9 +13,9 @@ import org.ahsan.swpark.domain.Memo;
 
 public class MemoDao {
 
-	private static final String URL = "jdbc:mysql://118.130.116.75:3306/madvirusdb?useUnicode=true&characterEncoding=utf-8";
-	String dbUser = "gnu";
-	String pass = "#2323#";
+	private static final String URL = "jdbc:mysql://localhost:3306/madvirusdb?useUnicode=true&characterEncoding=utf-8";
+	String dbUser = "madvirus";
+	String pass = "madvirus";
 	
 	public static final int MEMO_PER_PAGE = 15;
 	private Connection mConnection;
